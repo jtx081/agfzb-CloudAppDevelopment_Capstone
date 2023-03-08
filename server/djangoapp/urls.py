c7ace77ee23e992f69707b516b37d037872663d1
Joanna <jtx081@gmail.com>
2023-03-08 08:54:33 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,15 +16,19 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
+    
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
